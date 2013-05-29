61a9e84afb72c79fa18c7ddf0b58de38545423d8
Jason Morriss <lifo101@gmail.com>
2013-05-29 08:52:27 -0400

    [CiscoDevice] Override getOutput() to cleanup backspaces in output.

--- src/Lifo/RemoteControl/Type/CiscoDevice.php
+++ src/Lifo/RemoteControl/Type/CiscoDevice.php
@@ -31,6 +31,40 @@ class CiscoDevice extends NetworkDevice
         parent::__construct($options);
     }
 
+    /**
+     * Get the current output buffer.
+     *
+     * Try to reassemble text by intelligently removing backspaces from output.
+     */
+    public function getOutput()
+    {
+        $output = parent::getOutput();
+        if ($output !== null) {
+            $output = preg_replace(array(
+                '/\s{7}\x08+/',             // remove "       ^H^H^H^H^H^H^H^H" (from --More-- prompts)
+                '/\r\s{14}\r/',             // remove "              " (from <--- More ---> prompts)
+                '/\r/',                     // remove CR
+            ), '', $output);
+
+            // Only works for strings that only "loop" once on the cisco CLI....
+            // I'm working on it...  ugh, this is annoying.
+            $removeBackspaces = function($in) use (&$removeBackspaces) {
+                $out = preg_replace_callback('/^([^\x08]+)(\x08+)\$.(.)\s+([^\x08]+)?\x08+(.+)/', function($m) use (&$removeBackspaces) {
+                    $s = $m[1] . $m[3];
+                    if (!empty($m[4]) and substr($m[4], 0, 1) != "\x08") {
+                        $s .= $m[4];
+                    } else {
+                        $s .= $removeBackspaces($m[5]);
+                    }
+                    return $s;
+                }, $in);
+                return $out;
+            };
+            $output = implode("\n", array_map($removeBackspaces, explode("\n", $output)));
+        }
+        return $output;
+    }
+
     // @todo Implement cisco specific features like:
     //       reload(), copy(), dir(), etc...
 }
