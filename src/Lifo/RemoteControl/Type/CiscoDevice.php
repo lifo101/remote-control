21c254c7df54483f24b307b120c2e60e1ea7c6e7
Jason Morriss <lifo101@gmail.com>
2013-08-29 14:50:31 -0400

    [CiscoDevice] Fixed getOutput() to properly strip ^H from text. Added `normalize_output` option to disable/enable.

--- src/Lifo/RemoteControl/Type/CiscoDevice.php
+++ src/Lifo/RemoteControl/Type/CiscoDevice.php
@@ -27,6 +27,8 @@ class CiscoDevice extends NetworkDevice
     {
         $options = array_merge(array(
             'prompt' => '[a-zA-Z0-9._-]+ ?(\(config[^\)]*\))? ?[$#>] ?(\(enable\))? *$',
+            // Should output be normalized? (remove "More" prompts and fix backspaces, etc)
+            'normalize_output' => true,
         ), $options);
         parent::__construct($options);
     }
@@ -35,32 +37,123 @@ class CiscoDevice extends NetworkDevice
      * Get the current output buffer.
      *
      * Try to reassemble text by intelligently removing backspaces from output.
+     * This happens on Cisco devices when user input is echoed. If a long line
+     * of text is entered by the 'user' then the Cisco scrolls the prompt by
+     * using a lot of extra backspaces and spaces. This looks ugly in the
+     * captured output so this tries to remove the extra garbage w/o affecting
+     * the actual command text.
+     *
+     * This also removes "More" prompts.
+     *
+     * @param array|boolean $options Array of options or a boolean for the
+     *                               option "normalize_output".
      */
-    public function getOutput()
+    public function getOutput($options = array())
     {
         $output = parent::getOutput();
-        if ($output !== null) {
-            $output = preg_replace(array(
-                '/\s{7}\x08+/',             // remove "       ^H^H^H^H^H^H^H^H" (from --More-- prompts)
-                '/\r\s{14}\r/',             // remove "              " (from <--- More ---> prompts)
-                '/\r/',                     // remove CR
-            ), '', $output);
 
-            // Only works for strings that only "loop" once on the cisco CLI....
-            // I'm working on it...  ugh, this is annoying.
-            $removeBackspaces = function($in) use (&$removeBackspaces) {
-                $out = preg_replace_callback('/^([^\x08]+)(\x08+)\$.(.)\s+([^\x08]+)?\x08+(.+)/', function($m) use (&$removeBackspaces) {
-                    $s = $m[1] . $m[3];
-                    if (!empty($m[4]) and substr($m[4], 0, 1) != "\x08") {
-                        $s .= $m[4];
-                    } else {
-                        $s .= $removeBackspaces($m[5]);
-                    }
-                    return $s;
-                }, $in);
-                return $out;
-            };
-            $output = implode("\n", array_map($removeBackspaces, explode("\n", $output)));
+        $options = array_merge($this->options, is_array($options) ? $options : array('normalize_output' => $options));
+
+        if ($output !== null and $options['normalize_output']) {
+            // Remove "More" prompts
+            $output = str_replace("\r", "", preg_replace(array(
+                '/<--- More --->\r\s{14}\r/',       // cisco firewalls
+                '/ --More-- \x08{9}\s{8}\x08{9}/',  // cisco routers
+            ), '', $output));
+
+            $str = '';
+            foreach (explode("\n", $output) as $line) {
+                // short-cut; no "^H" (backspaces) found so just continue
+                if (strpos($line, "\x08") == -1) {
+                    $str .= $line . "\n";
+                    continue;
+                }
+
+                $parts = array();
+                $idx = -1;
+                $bs_length = 0;
+                $bs_length_first = 0;
+                $forward = '';
+
+                // loop through each character and act accordingly ...
+                $state = 'NORMAL';
+                foreach (str_split($line) as $c) {
+                    $idx ++;
+                    // The order of the case statements below are crucial!
+                    switch ($state) {
+                        // In NORMAL state we simply eat characters until we
+                        // see a ^H (backspace) character.
+                        case 'NORMAL':
+                            if ($c !== "\x08") {
+                                $str .= $c;
+                                break;
+                            } else {
+                                $parts[] = $str;
+                                $str = '';
+                                $state = 'REVERSE1';
+                                // fallthru to REVERSE1
+                            }
+
+                        // REVERSE1 state occurs after NORMAL and we eat ^H
+                        // characters until we hit a normal character.
+                        case 'REVERSE1':
+                            if ($c === "\x08") {
+                                break;
+                            } else {
+                                $forward = '';
+                                $state = 'FORWARD';
+                                // fallthru to FORWARD
+                            }
+
+                        // FORWARD state occurs after REVERSE1 and eats chars
+                        // until a ^H is seen.
+                        case 'FORWARD':
+                            if ($c !== "\x08") {
+                                $forward .= $c;
+                                break;
+                            } else {
+                                $state = 'REVERSE2';
+                                // fallthru to REVERSE2
+                            }
+
+                        // REVERSE2 state occurs after FORWARD and eats ^H
+                        // until we hit a normal character.
+                        case 'REVERSE2':
+                            if ($c === "\x08") {
+                                $bs_length ++;
+                            } else {
+                                if (!$bs_length_first) {
+                                    $bs_length_first = $bs_length;
+                                }
+
+                                // get the last character
+                                $str .= substr($forward, -$bs_length_first-1, 1);
+
+                                // when the ^H lengths are not the same it tells
+                                // us we're at the end of the user text and we
+                                // can ignore the remaining input on the line.
+                                if ($bs_length_first != $bs_length) {
+                                    $parts[] = $str;
+                                    $str = '';
+                                    break 2; // break out of switch and foreach
+                                }
+
+                                $str .= $c;
+                                $bs_length = 0;
+                                $state = 'NORMAL';
+                            }
+                            break;
+
+                        default: // failsafe against regression bugs; should never occur
+                            throw new \Exception("Unknown state ($state) for text normalization.");
+                    } # switch
+
+                } # foreach char
+
+                $str .= implode('', $parts) . "\n";
+            } # foreach line
+
+            return $str;
         }
         return $output;
     }
